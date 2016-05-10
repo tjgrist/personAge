99a5792b1516e6d3fc87abe5172a1fc4b1190cc7
tjgrst <tjgrst@gmail.com>
2016-05-10 12:08:33 -0500

    intitial commit

---
+++ AgePerson/Person.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AgePerson
+{
+    class Person
+    {
+        public string firstName;
+        public string lastName;
+        int _age;
+        int birthyear;
+
+        public int Age
+        {
+            get { return _age; }
+            set { _age = value; }
+        }
+        public Person(int birthyear)
+        {
+            this.birthyear = birthyear;
+            _age = 2016 - birthyear;
+            Console.Write("Enter your name: ");
+            string[] fullName = Console.ReadLine().Split(' ');
+            firstName = fullName[0];
+            lastName = fullName[1];
+        }
+        public static Person operator +(Person person, int number)
+        {
+            person._age += number;
+            return person;
+        }
+    }
+}
---
+++ AgePerson/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AgePerson
+{
+    class Program
+    {
+ 
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What year were you born?");
+            int birthyear = Convert.ToInt32(Console.ReadLine());
+            Person person = new Person(birthyear);
+            person = person + 35; //this is overloading the + operator 
+            Console.WriteLine("{0} {1} will be {2} in 2051.", person.firstName, person.lastName, person.Age);
+            Console.ReadKey();
+        }
+    }
+}
